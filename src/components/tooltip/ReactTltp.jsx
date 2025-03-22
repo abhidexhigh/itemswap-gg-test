aca3a72bfa914ea0216ae27600104c36590e3829
Abhishek Kumar <abhishek.mishra@dexhigh.com>
2025-03-22 12:31:29 +0530

    Tooltip content fixes

--- src/components/tooltip/ReactTltp.jsx
+++ src/components/tooltip/ReactTltp.jsx
@@ -1,4 +1,5 @@
 import { Tooltip } from "react-tooltip";
+import Image from "next/image";
 import "react-tooltip/dist/react-tooltip.css";
 import Comps from "../../data/compsNew.json";
 import { useEffect, useState, useRef } from "react";
@@ -296,9 +297,14 @@ const ReactTltp = ({ variant = "", content, id }) => {
             <div className="flex justify-start items-center gap-x-2">
               {content?.name}
               <span className="flex justify-center items-center">
-                <img
+                <Image
                   src="https://res.cloudinary.com/dg0cmj6su/image/upload/v1720771035/dollar_i5ay9s.png"
                   className="w-3"
+                  alt="dollar"
+                  width={24}
+                  height={24}
+                  placeholder="blur"
+                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
                 />
                 {content?.cost}
               </span>
@@ -306,34 +312,51 @@ const ReactTltp = ({ variant = "", content, id }) => {
             <div className="mb-2">
               {content?.traits.map((trait, index) => (
                 <div className="flex justity-left items-center" key={index}>
-                  <img
+                  <Image
                     src={traits?.find((t) => t?.key === trait)?.imageUrl}
                     className="w-5"
                     alt={trait}
+                    width={24}
+                    height={24}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
                   />
                   <span className="text-xs font-light">{trait}</span>
                 </div>
               ))}
             </div>
-            <div className="flex justify-start items-center text-xs font-light mb-2">
-              <div>Attack Range: </div>
-              <div>{content?.attackRange}</div>
-            </div>
             <div className="flex justify-start items-center gap-x-2">
-              <img src={content?.skill?.imageUrl} className="w-6" alt="skill" />
-              <div className="text-xs font-light">
-                <div>{content?.skill?.name}</div>
-                <div>Mana: {content?.skill?.skillMana}</div>
+              <div>Abilities</div>
+              <div className="flex justify-start items-center gap-x-2">
+                {content?.abilities?.map((ability, index) => (
+                  <div key={index}>
+                    <Image
+                      src={ability?.imageUrl}
+                      alt="skill"
+                      width={24}
+                      height={24}
+                      placeholder="blur"
+                      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
+                      className="w-8 rounded-sm !border !border-white/60"
+                    />
+                  </div>
+                ))}
               </div>
             </div>
             <div className="text-[10px] text-left font-light mb-1">
               {content?.skill?.desc}
             </div>
             <div className="text-xs text-left font-light">
+              Attack Range: {content?.attackRange}
+            </div>
+            <div className="text-xs text-left font-light">
               Attack Speed: {content?.attackSpeed}
             </div>
             <div className="text-xs text-left font-light mb-1">
-              Damage: {content?.damagePerSecond}
+              Damage: {content?.attackDamage}
+            </div>
+            <div className="text-xs text-left font-light mb-1">
+              Mana: {content?.mana}
             </div>
             <div>
               <div className="text-xs text-left font-light">
@@ -341,11 +364,15 @@ const ReactTltp = ({ variant = "", content, id }) => {
               </div>
               <div className="flex justify-start items-center gap-x-1">
                 {content?.recommendItems?.map((item, index) => (
-                  <img
+                  <Image
                     key={index}
                     src={items?.find((i) => i?.key === item)?.imageUrl}
-                    className="w-8"
                     alt={item}
+                    width={24}
+                    height={24}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
+                    className="w-8 rounded-sm !border !border-white/60"
                   />
                 ))}
               </div>
@@ -365,10 +392,14 @@ const ReactTltp = ({ variant = "", content, id }) => {
                 content?.compositions?.map((comp, index) => (
                   <div className="flex justify-center items-center" key={index}>
                     <div className="rounded-full">
-                      <img
+                      <Image
                         src={
                           items?.find((item) => item?.key === comp)?.imageUrl
                         }
+                        width={24}
+                        height={24}
+                        placeholder="blur"
+                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
                         className="w-10 mr-1"
                         alt={comp}
                       />
@@ -417,10 +448,14 @@ const ReactTltp = ({ variant = "", content, id }) => {
             <div className="grid grid-cols-4 justify-center items-center gap-x-4">
               {content?.map((trait, index) => (
                 <div key={index}>
-                  <img
+                  <Image
                     src={traits?.find((t) => t?.key === trait?.name)?.imageUrl}
                     className="w-14"
                     alt={trait?.name}
+                    width={24}
+                    height={24}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/SkhXQAAAABJRU5ErkJggg=="
                   />
                   <div className="text-sm text-center font-light">
                     {trait?.name}
